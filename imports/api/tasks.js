29a3533ef2db09e6ae43c9b12008d76961c77137
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-11 15:13:40 +0000

    Change the function to adjust to the test environment

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -49,7 +49,7 @@ Meteor.methods({
       check(setChecked, Boolean);
  
       const task = Tasks.findOne(taskId);
-      if (task.private && task.owner !== Meteor.userId()) {
+      if (task.private && task.owner !== this.userId) {
         // If the task is private, make sure only the owner can check it off
         throw new Meteor.Error('not-authorized');
       }
@@ -63,7 +63,7 @@ Meteor.methods({
       const task = Tasks.findOne(taskId);
    
       // Make sure only the task owner can make a task private
-      if (task.owner !== Meteor.userId()) {
+      if (task.owner !== this.userId) {
         throw new Meteor.Error('not-authorized');
       }
    

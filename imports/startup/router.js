4e99245e7b2e61d3135e320b353c7764a767e817
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-15 11:42:11 +0000

    Adding if statement to check if user is connected or not

--- imports/startup/router.js
+++ imports/startup/router.js
@@ -2,10 +2,20 @@
 
 FlowRouter.route('/',{
     action:function(){
-        BlazeLayout.render('App_Body',
-        {
-            //header:'Landing_page',
-            main:'Landing_page'
-        });
+        if(Meteor.userId()){
+            //If logged in
+            BlazeLayout.render('App_Body',
+            {
+                //header:'Landing_page',
+                main:'Landing_page'
+            }); 
+        }else{
+            //If NOT logged in
+            BlazeLayout.render('App_Body',
+            {
+                //header:'Landing_page',
+                main:'Prompt_page'
+            });
+        }
     }
 });

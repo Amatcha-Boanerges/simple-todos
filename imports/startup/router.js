f02028505382c32cf613b3ffdf8c5d4eb239886d
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-15 14:38:02 +0000

    Add a function to refresh the page as soon as we login

--- imports/startup/router.js
+++ imports/startup/router.js
@@ -2,20 +2,38 @@
 
 FlowRouter.route('/',{
     action:function(){
-        if(Meteor.userId()){
-            //If logged in
-            BlazeLayout.render('App_Body',
-            {
-                //header:'Landing_page',
-                main:'Landing_page'
-            }); 
-        }else{
+        // if(Meteor.userId()){
+        //     //If logged in
+        //     BlazeLayout.render('App_Body',
+        //     {
+        //         //header:'Landing_page',
+        //         main:'Landing_page'
+        //     }); 
+        // }else{
             //If NOT logged in
             BlazeLayout.render('App_Body',
             {
                 //header:'Landing_page',
                 main:'Prompt_page'
             });
-        }
+        //}
+    }
+});
+
+FlowRouter.route('/tasks',{
+    action:function(){
+        BlazeLayout.render('App_Body',
+        {
+            //header:'Landing_page',
+            main:'Landing_page'
+        });
+    }
+});
+
+FlowRouter.route('/logout',{
+    action:function(){
+        Meteor.logout(function(){
+            FlowRouter.go('/');
+        });
     }
 });

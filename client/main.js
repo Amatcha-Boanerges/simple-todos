3bd3768ae2d675cadd24e3dca526dc1ce5b9db4d
Amatcha-Boanerges <jacquesyann@live.fr>
2019-01-15 10:47:09 +0000

    update to add routing feature

--- client/main.js
+++ client/main.js
@@ -1,2 +1,9 @@
+import {Template} from 'meteor/templating';
+
 import '../imports/startup/accounts-config.js';
-import '../imports/ui/body.js';
+
+//include routing
+import '../imports/startup/router.js';
+
+//Include pages
+import '../imports/ui/pages/landing.js'
